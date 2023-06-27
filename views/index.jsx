e796cc418db9c342d1a1d34388332241b06d0a42
Cody Jarrett <cjarrett@newrelic.com>
2023-06-27 14:36:52 -0500

    create and new routes complete

--- views/index.jsx
+++ views/index.jsx
@@ -7,6 +7,10 @@ function Index({breads, title}){
     return (
       <Default title={title}>
         <h2>Index Page</h2>
+        <div className="newButton">
+          <a href="/breads/new"><button>Add a new bread</button></a>
+        </div>
+
         <ul>
             {/* {
               breads.map(bread => {
